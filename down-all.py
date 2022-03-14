dfe564eb7ffb58370778ad8a16ef70627f79ca27
一梦不醒 <41280135+1094093288@users.noreply.github.com>
2022-03-14 17:46:04 +0800

    add execCmd

--- down-all.py
+++ down-all.py
@@ -1,4 +1,18 @@
-import os
+import os, re
+
+# execute command, and return the output
+
+def execCmd(cmd):
+
+	r = os.popen(cmd)
+
+	text = r.read()
+
+	r.close()
+
+	return text
+
+# write "data" to file-filename
 
 def lg(s):
 	print('\033[1;31;40m[+]  %s '%(s))
@@ -7,7 +21,8 @@ def download(LibcNameList):
     for item in LibcNameList:
         os.system("./download {}".format(item))
         
-val = os.system("ls libs/ | wc -l ")
+val = int(execCmd("ls libs/ | wc -l "))
+
 if val == 0:
 	os.system("python update_list")
 	lg("update list finished!!")
